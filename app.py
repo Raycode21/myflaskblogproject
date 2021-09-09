c456291d9b3dc926c480522d5799d26c5beced31
USER <wanjiku.rachel21@gmail.com>
2021-09-09 09:26:37 +0300

    initial changs to templates

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2d5ca6681f533eeb43d8c4d92e3a9b87'
 
 posts = [
 {
@@ -27,6 +29,16 @@ def home():
 @app.route("/about")
 def about():
 	return render_template("about.html", title='About')	
+
+@app.route("/register")
+def register():	
+	form = RegistrationForm()
+	return render_template("register.html", title='Register', form=form)
+
+@app.route("/Login")
+def register():	
+	form = LoginForm()	
+	return render_template("Login.html", title='Login', form=form)
 
 if __name__ == "__main__":
 	app.run(debug=True)
